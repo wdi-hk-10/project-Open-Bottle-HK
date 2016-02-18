e0abb2322c9319981a907ec9f795e7dc9d18d399
cmtaubman <carmen@cgkcorporation.com>
2016-02-18 17:10:16 +0800

    next change

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ var plugins = [
   { register: require('vision')}, // views
   { register: require('inert')}, // public files hosting
   { register: require('./routes/static_pages.js')},
-  { register: require('./routes/auth.js')},
+  // { register: require('./routes/auth.js')},
   { register: require('./routes/api/auth.js')},
   { register: require('hapi-mongodb'), // setup the mongo connect
     options: {
-      "url": process.env.MONGOLAB_URI || "mongodb://127.0.0.1:27017/hapi-template", // CHANGE-ME
+      "url": process.env.MONGOLAB_URI || "mongodb://127.0.0.1:27017/hapi-obhk", // CHANGE-ME
       "settings": {
         "db": {
           "native_parser": false
