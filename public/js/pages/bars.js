3d4b80688bca95a48bc26c165ff08f0d8c0eb15a
cmtaubman <carmen@cgkcorporation.com>
2016-02-22 22:41:21 +0800

    bar csv changes

---
+++ public/js/pages/bars.js
@@ -0,0 +1,10 @@
+// google map functionality
+function initMap() {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+      center: {lat: 22.278417, lng: 114.170817},
+      zoom: 16
+    });
+  }
+
+initmap();
